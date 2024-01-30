0cc53072509f59813d8481d65698d1e01bc684bd
ab0111 <118175398+ab0111@users.noreply.github.com>
2024-01-30 17:50:22 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,51 @@
+
+var deButton = document.querySelector("header button");
+
+
+deButton.onclick = toggleMenu;
+
+
+function toggleMenu() {  
+
+  var debody = document.querySelector("body");
+
+  debody.classList.toggle("toonMenu");
+  
+}
+
+window.onkeydown = handleKeydown;
+
+function handleKeydown(event) {
+  if (event.key == "Escape") {
+    var deNav = document.querySelector("nav");
+    deNav.classList.remove("toonMenu");
+  }
+}
+
+
+
+
+// var deButton = document.getElementById("search-button");
+// var deInput = document.getElementById("search-input");
+
+// deButton.onclick = toggleMenu;
+
+// function toggleMenu() {  
+//   var deBody = document.querySelector("body");
+//   deBody.classList.toggle("toonMenu");
+  
+ 
+//   var expanded = deButton.getAttribute("aria-expanded") === "true";
+//   deButton.setAttribute("aria-expanded", !expanded);
+// }
+
+// window.onkeydown = handleKeydown;
+
+// function handleKeydown(event) {
+//   if (event.key == "Escape") {
+//     var deNav = document.querySelector("nav");
+//     deNav.classList.remove("toonMenu");
+//   }
+// }
+
+/* Bronnen: https://codepen.io/shooft/pen/ZEVYyMQ*/
